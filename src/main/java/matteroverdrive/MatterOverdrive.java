72f0bc562767fe348e4a0a6f0926d341bca65292
infiniteblock <subaru_gt@windowslive.com>
2024-03-10 14:51:47 +1300

    Update MatterOverdrive.java
    Update Icon's

--- src/main/java/matteroverdrive/MatterOverdrive.java
+++ src/main/java/matteroverdrive/MatterOverdrive.java
@@ -60,9 +60,9 @@ public class MatterOverdrive {
 	public static final OverdriveTab TAB_OVERDRIVE_FOOD = new OverdriveTab("tabMO_food",
 			() -> new ItemStack(ITEMS.earl_gray_tea));
 	public static final OverdriveTab TAB_OVERDRIVE_SHIPS = new OverdriveTab("tabMO_ships",
-			() -> new ItemStack(ITEMS.contract));
+			() -> new ItemStack(ITEMS.colonizerShip));
 	public static final OverdriveTab TAB_OVERDRIVE_BUILDINGS = new OverdriveTab("tabMO_buildings",
-			() -> new ItemStack(ITEMS.contract));
+			() -> new ItemStack(ITEMS.buildingBase));
 	public static final OverdriveTab TAB_OVERDRIVE_DECORATIVE = new OverdriveTab("tabMO_decorative",
 			() -> new ItemStack(BLOCKS.decorative_tritanium_plate));
 
