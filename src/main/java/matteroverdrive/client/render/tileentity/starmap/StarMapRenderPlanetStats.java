1df69ae8bee8ddad3671a1384fa3ac7c2ded04d7
infiniteblock <subaru_gt@windowslive.com>
2024-03-12 16:21:22 +1300

    Update StarMapRenderPlanetStats.java

--- src/main/java/matteroverdrive/client/render/tileentity/starmap/StarMapRenderPlanetStats.java
+++ src/main/java/matteroverdrive/client/render/tileentity/starmap/StarMapRenderPlanetStats.java
@@ -1,14 +1,21 @@
 
 package matteroverdrive.client.render.tileentity.starmap;
 
+import matteroverdrive.Reference;
+import matteroverdrive.api.starmap.IShip;
+import matteroverdrive.client.data.Color;
 import matteroverdrive.proxy.ClientProxy;
 import matteroverdrive.starmap.data.Galaxy;
 import matteroverdrive.starmap.data.Planet;
 import matteroverdrive.starmap.data.SpaceBody;
+import matteroverdrive.starmap.data.TravelEvent;
 import matteroverdrive.tile.TileEntityMachineStarMap;
+import matteroverdrive.util.MOStringHelper;
 import matteroverdrive.util.RenderUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.item.ItemStack;
+
 import org.lwjgl.util.vector.Matrix4f;
 import org.lwjgl.util.vector.Vector3f;
 import org.lwjgl.util.vector.Vector4f;
@@ -73,19 +80,52 @@ public class StarMapRenderPlanetStats extends StarMapRendererPlanet {
 		}
 	}
 
+    protected void drawTravelingShips(Galaxy galaxy,Planet planet)
+    {
+        int heightCount = -24;
+        for (TravelEvent travelEvent : galaxy.getTravelEvents())
+        {
+            if (travelEvent.getTo().equals(planet) && ((IShip)travelEvent.getShip().getItem()).isOwner(travelEvent.getShip(), Minecraft.getMinecraft().player))
+            {
+                String time = MOStringHelper.formatRemainingTime(travelEvent.getTimeRemainning(Minecraft.getMinecraft().world) / 20);
+                int width = fontRenderer.getStringWidth(time);
+                RenderUtils.renderStack(-8, heightCount - 8,0, travelEvent.getShip(),false);
+                fontRenderer.drawString(time, -width / 2, heightCount + 8, Reference.COLOR_HOLO.getColor());
+                heightCount -= 26;
+            }
+        }
+    }
+
 	@Override
 	protected void drawPlanetInfoClose(Planet planet) {
 
 	}
 
-	@Override
-	public void renderGUIInfo(Galaxy galaxy, SpaceBody spaceBody, TileEntityMachineStarMap starMap, float partialTicks,
-			float opacity) {
-		if (spaceBody instanceof Planet) {
-			Planet planet = (Planet) spaceBody;
-			int y = 0;
-		}
-	}
+    @Override
+    public void renderGUIInfo(Galaxy galaxy, SpaceBody spaceBody,TileEntityMachineStarMap starMap, float partialTicks, float opacity)
+    {
+        if (spaceBody instanceof Planet)
+        {
+            Planet planet = (Planet)spaceBody;
+            int y = 0;
+            for (ItemStack shipStack : planet.getFleet())
+            {
+                if (shipStack.getItem() instanceof IShip)
+                {
+                    IShip ship = (IShip)shipStack.getItem();
+                    Color shipColor = Reference.COLOR_HOLO;
+                    if (!ship.isOwner(shipStack, Minecraft.getMinecraft().player))
+                    {
+                        shipColor = Reference.COLOR_HOLO_RED;
+                    }
+
+                    RenderUtils.renderStack(16, y - 16,0, shipStack,false);
+                    fontRenderer.drawString(shipStack.getDisplayName(), 36, y - 10, shipColor.getColor());
+                    y -= 16;
+                }
+            }
+        }
+    }
 
 	@Override
 	public boolean displayOnZoom(int zoom, SpaceBody spaceBody) {
